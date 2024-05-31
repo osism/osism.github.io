a6aee51f786bb11f4eb51631b5484cc4e43ce5af
berendt <berendt@users.noreply.github.com>
2024-05-31 20:30:45 +0000

    deploy: 8a9fcb730d4dbd9f33f8d936b455733850aa25f8

---
+++ de/assets/js/80f98674.9a324934.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkosism=self.webpackChunkosism||[]).push([[9632],{3651:e=>{e.exports=JSON.parse('{"tag":{"label":"News","permalink":"/de/blog/tags/news","allTagsPath":"/de/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/de/blog/tags/news","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ de/assets/js/aa90c9d1.4f3fd957.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkosism=self.webpackChunkosism||[]).push([[9514],{2091:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/de/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":6,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
