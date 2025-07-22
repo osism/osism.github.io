943e76591262a9b5509afaad8a4aadc412c30d2f
berendt <berendt@users.noreply.github.com>
2025-07-22 10:03:38 +0000

    deploy: ed0a21ae9e285bf0d2bade9e352cb66da67aaa1b

--- assets/js/5e95c892.6cb6ca25.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkosism=self.webpackChunkosism||[]).push([[9647],{7121:(s,e,r)=>{r.r(e),r.d(e,{default:()=>l});r(6540);var u=r(4164),a=r(5500),c=r(7559),n=r(2831),t=r(4514),i=r(4848);function l(s){return(0,i.jsx)(a.e3,{className:(0,u.A)(c.G.wrapper.docsPages),children:(0,i.jsx)(t.A,{children:(0,n.v)(s.route.routes)})})}}}]);
